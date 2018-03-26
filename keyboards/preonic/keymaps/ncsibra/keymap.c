4de0450685aa6e1f3c702e0518deca32664fc6f6
Norbert Csibra <csn1984@gmail.com>
2018-03-26 21:10:32 +0200

    Update preonic layer

--- keyboards/preonic/keymaps/ncsibra/keymap.c
+++ keyboards/preonic/keymaps/ncsibra/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,         KC_Q,     KC_W,     KC_E,           KC_R,         KC_T,    KC_Y,    KC_U,        KC_I,     KC_O,     KC_P,     KC_QUOT},
   {KC_ESC,         KC_A,     KC_S,     KC_D,           KC_F,         KC_G,    KC_H,    KC_J,        KC_K,     KC_L,     KC_SCLN,  KC_ENT},
   {OSM(MOD_LSFT),  KC_Z,     KC_X,     KC_C,           KC_V,         KC_B,    KC_N,    KC_M,        KC_COMM,  KC_DOT,   KC_SLSH,  OSM(MOD_RSFT)},
-  {TT(_NUM),       KC_LGUI,  KC_LALT,  CTL_T(KC_DEL),  OSL(_LOWER),  KC_SPC,  KC_BSPC, TT(_RAISE),  KC_RCTRL, KC_RALT,  TG(_NUM), KC_LEAD}
+  {TT(_NUM),       KC_LGUI,  KC_LALT,  CTL_T(KC_DEL),  OSL(_LOWER),  KC_SPC,  KC_BSPC, TT(_RAISE),  KC_RCTRL, KC_RALT,  TO(_NUM), KC_LEAD}
 },
 
 /* Lower
