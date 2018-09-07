b41f61f05de94fe8c4c761c8adff837f0818e68a
Norbert Csibra <csn1984@gmail.com>
2018-09-07 19:18:13 +0200

    Add atreus62 support

---
+++ keyboards/atreus62/keymaps/ncsibra/keymap.c
@@ -0,0 +1,24 @@
+#include "ncsibra.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+[_COLEMAK] = VA_KEYMAP(COLEMAK_LAYER, BLANK_MID),
+
+[_QWERTY] = VA_KEYMAP(QWERTY_LAYER, BLANK_MID),
+
+[_LOWER] = VA_KEYMAP(LOWER_LAYER, BLANK_MID),
+
+[_RAISE] = VA_KEYMAP(RAISE_LAYER, BLANK_MID),
+
+[_NUM] = VA_KEYMAP(NUM_LAYER, BLANK_MID),
+
+[_VIM_N] = VA_KEYMAP(VIM_NORMAL_LAYER, BLANK_MID),
+
+[_VIM_V] = VA_KEYMAP(VIM_INSERT_LAYER, BLANK_MID),
+};
+
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+  return true;
+};
+
+void matrix_scan_keymap(void) {};
