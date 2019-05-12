3b3fd7f3205c75b1cda183d1e1974fc120dd815f
Norbert Csibra <csn1984@gmail.com>
2019-05-12 09:50:08 +0200

    Update viterbi

--- keyboards/keebio/viterbi/keymaps/ncsibra/rules.mk
+++ keyboards/keebio/viterbi/keymaps/ncsibra/rules.mk
@@ -11,3 +11,5 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 API_SYSEX_ENABLE = no
+
+SPLIT_KEYBOARD = yes
