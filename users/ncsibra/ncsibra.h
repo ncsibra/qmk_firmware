ca6d5e906ff246d5c2e3b6be375e776a669aaa97
Norbert Csibra <csn1984@gmail.com>
2019-12-11 22:11:31 +0100

    Update num layer

--- users/ncsibra/ncsibra.h
+++ users/ncsibra/ncsibra.h
@@ -134,10 +134,10 @@ enum keycodes {
 
 #define NUM_LAYER \
   XXXXXXX,       XXXXXXX,     XXXXXXX,     XXXXXXX,       XXXXXXX,     KC_VOLU,       XXXXXXX,  KC_NUMLOCK,  KC_KP_EQUAL,  KC_KP_SLASH,  KC_KP_ASTERISK,  KC_BSPC, \
-  LCTL(KC_TAB),  XXXXXXX,     XXXXXXX,     XXXXXXX,       XXXXXXX,     KC_VOLD,       XXXXXXX,  KC_KP_7,     KC_KP_8,      KC_KP_9,      KC_KP_MINUS,     XXXXXXX, \
-  KC_VOLU,       LCTL(KC_A),  LCTL(KC_S),  XXXXXXX,       LCTL(KC_F),  XXXXXXX,       XXXXXXX,  KC_KP_4,     KC_KP_5,      KC_KP_6,      KC_KP_PLUS,      KC_KP_ENTER, \
-  KC_VOLD,       LCTL(KC_Z),  LCTL(KC_X),  LCTL(KC_INS),  XXXXXXX,     LSFT(KC_INS),  XXXXXXX,  KC_KP_1,     KC_KP_2,      KC_KP_3,      XXXXXXX,         XXXXXXX, \
-  _______,       _______,     _______,     TO(_COLEMAK),  XXXXXXX,     KC_SPC,        KC_BSPC,  KC_KP_0,     KC_KP_DOT,    KC_KP_DOT,    TO(_COLEMAK),    XXXXXXX
+  XXXXXXX,       XXXXXXX,     XXXXXXX,     XXXXXXX,       XXXXXXX,     KC_VOLD,       XXXXXXX,  KC_KP_7,     KC_KP_8,      KC_KP_9,      KC_KP_MINUS,     XXXXXXX, \
+  KC_VOLU,       XXXXXXX,     XXXXXXX,     XXXXXXX,       XXXXXXX,     XXXXXXX,       XXXXXXX,  KC_KP_4,     KC_KP_5,      KC_KP_6,      KC_KP_PLUS,      KC_KP_ENTER, \
+  KC_VOLD,       XXXXXXX,     XXXXXXX,     XXXXXXX,       XXXXXXX,     LSFT(KC_INS),  XXXXXXX,  KC_KP_1,     KC_KP_2,      KC_KP_3,      XXXXXXX,         XXXXXXX, \
+  _______,       _______,     _______,     XXXXXXX,       XXXXXXX,     XXXXXXX,       KC_BSPC,  KC_KP_0,     KC_KP_DOT,    KC_KP_DOT,    XXXXXXX,         XXXXXXX
 
 
 #define VIM_NORMAL_LAYER \
